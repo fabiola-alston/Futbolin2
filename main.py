2e7d5a2bdd07b21e33ac8767684a3fe4e376c784
fabiola-alston <f.alston.1@estudiantec.cr>
2024-05-26 00:53:03 -0600

    whole goal system

--- main.py
+++ main.py
@@ -1,55 +1,119 @@
+import random
 import time
 from tkinter import *
 from tkmacosx import *
 from PIL import Image, ImageTk
 import pygame
 
+# music
 pygame.mixer.init()
-music = pygame.mixer.music.load("Sounds/music.mp3")
-pygame.mixer.music.play(-1)
+# music = pygame.mixer.music.load("Sounds/music.mp3")
+# pygame.mixer.music.play(-1)
 
+# window creation
 window = Tk()
 window.title("FIFA '95")
 window.geometry("800x600")
 window.configure(bg="black")
 
+# setting game canvas
 game_canvas = Canvas(window, bg="#6eadff", width=400, height=300, highlightthickness=0)
 game_canvas.place(relx=0.5, rely=0.4, anchor=CENTER)
 
+
+# ball class
 class Ball:
     def __init__(self, image):
         self.image = image
 
-    def moveBall(self):
+    # function to animate ball
+    def moveBall(self, direction):
+        sound = pygame.mixer.Sound("Sounds/pickupCoin.wav")
+        pygame.mixer.Sound.play(sound)
+
+        shoot_button["state"] = "disabled"
+
+        if direction == 1:
+            x = -25
+            y = 18
+        elif direction == 2:
+            x = -15
+            y = 18
+        elif direction == 3:
+            x = -5
+            y = 18
+        elif direction == 4:
+            x = 5
+            y = 18
+        elif direction == 5:
+            x = 15
+            y = 18
+        elif direction == 6:
+            x = 25
+            y = 18
+
         for i in range(5):
-            game_canvas.move(self.image, 0, -10)
+            game_canvas.move(self.image, x, -y)
             time.sleep(0.05)
             window.update()
+        time.sleep(1)
+        for i in range(5):
+            game_canvas.move(self.image, -x, y)
+            time.sleep(0.05)
+            window.update()
+        shoot_button["state"] = "normal"
+
+    def shootBall(self):
+        random_block = random.randint(1,6)
+        x = 50 + (50 * (random_block - 1))
+
+        blocked_img = Image.open("Images/blocked.png")
+        blocked_tk = ImageTk.PhotoImage(blocked_img)
+
+        blocked_area = game_canvas.create_image((x, 80), anchor=NW, image=blocked_tk)
+
+        random_goal = random.randint(1, 6)
+
+        self.moveBall(random_goal)
+
+        if random_goal == random_block:
+            print(f"NO GOAL: Blocked: {random_block}")
+        else:
+            print(f"GOAL: Blocked: {random_block} Goal: {random_goal}")
+
+        game_canvas.delete(blocked_area)
 
 
+
+# goal field image load
 goal_field_img = Image.open("Images/goal_field.png")
 goal_field_img = goal_field_img.resize((400, 266), 5)
 goal_field_tk = ImageTk.PhotoImage(goal_field_img)
 
+# goalie image load
 goalie_img = Image.open("Images/goalie2.png")
 goalie_tk = ImageTk.PhotoImage(goalie_img)
 
+# ball image load
 ball_img = Image.open("Images/soccer_ball.png")
 ball_img = ball_img.resize((40, 40), 5)
 ball_tk = ImageTk.PhotoImage(ball_img)
 
-
+# setting image objects
 field = game_canvas.create_image((0, 35), image=goal_field_tk, anchor=NW)
 goalie = game_canvas.create_image((162, 160), image=goalie_tk, anchor=NW)
 ball = game_canvas.create_image((180, 250), image=ball_tk, anchor=NW)
 
+# ball object from class Ball
 ball = Ball(ball)
 
-shoot_button = Button(window, text="Shoot", font=("retro gaming", 14), bg="blue", fg="white", focuscolor="blue", borderless=1, borderwidth=2, highlightthickness=3, relief="raised", command=ball.moveBall)
+# shoot button
+shoot_button = Button(window, text="Shoot", font=("retro gaming", 14), bg="blue", fg="white", focuscolor="blue", borderless=1, borderwidth=2, highlightthickness=3, relief="raised", command=ball.shootBall)
 shoot_button.place(relx=0.5, rely=0.8, anchor=CENTER)
 
-# play_button = Button(window, text="Play", font=font1, borderless=1, borderwidth=2, highlightthickness=3, relief="raised",
-#                          bg="#ff0000", fg="white", overbackground="#ff5252", focuscolor="#ff0000", activebackground="#ff5252", padx=3, command=gameScreen)
-#     play_button.place(relx=0.5, rely=0.65, anchor=CENTER)
+# score text (label)
+score_label = Label(window, text="SCORE: 0", font=("retro gaming", 14), bg="black", fg="white")
+score_label.place(relx=0.3, rely=0.1, anchor=CENTER)
 
+# run window
 window.mainloop()
